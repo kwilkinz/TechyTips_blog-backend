a431ec7e0fe9f869dcaa263827e4c243eeb85c0e
Keri Wilkins <keriannwilkins@gmail.com>
2023-01-17 15:50:08 -0600

    update cors

--- backend/api/server.js
+++ backend/api/server.js
@@ -11,7 +11,11 @@ const cors = require("cors");
 
 const app = express();
 dotenv.config();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
