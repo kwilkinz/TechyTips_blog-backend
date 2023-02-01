a4eb0da7372d3a9c53aaaf3b0d5018cb0b7b3f09
Keri Wilkins <keriannwilkins@gmail.com>
2023-02-01 10:17:11 -0600

    removed api/ from uploads

--- backend/api/server.js
+++ backend/api/server.js
@@ -34,7 +34,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
